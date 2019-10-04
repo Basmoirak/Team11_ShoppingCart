fade97e99d752e4cd871387c552292582e1927c4
basmoirak <daniel_yn@yahoo.com.sg>
2019-10-04 16:07:46 +0800

    Added ClearShoppingCart method into BasketService

--- Team11_CA/Team11_CA/Shop.Services/BasketService.cs
+++ Team11_CA/Team11_CA/Shop.Services/BasketService.cs
@@ -166,5 +166,12 @@ namespace Team11_CA.Shop.Services
                 return model;
             }
         }
+
+        public void ClearBasket(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            basket.BasketItems.Clear();
+            basketContext.Commit();
+        }
     }
 }
